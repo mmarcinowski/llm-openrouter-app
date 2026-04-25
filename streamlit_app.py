f9b4d7be98f48a0c9c271b4bd0931cf922322183
Maks Marcinowski <ksymilian@gmail.com>
2026-04-25 08:43:24 +0200

    data update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,21 +1,16 @@
 import streamlit as st
 from openai import OpenAI
 import os
-import json
-#from langchain_openai.chat_models import ChatOpenAI
 
 st.set_page_config(layout="wide", page_title="OpenRouter chatbot app")
 st.title("OpenRouter chatbot app")
 
 # api_key, base_url = os.environ["API_KEY"], os.environ["BASE_URL"]
 api_key, base_url = st.secrets["API_KEY"], st.secrets["BASE_URL"]
-selected_model = "google/gemma-3-1b-it:free"
+selected_model = "gemini-2.5-flash"
 
 if "messages" not in st.session_state:
-    st.session_state["messages"] = [
-        {"role": "system", "content": "You are a helpful assistant."},
-        {"role": "user", "content": f"You are a helpful assistant."},
-    ]
+    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?."}]
 
 for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
@@ -34,19 +29,3 @@ if prompt := st.chat_input():
     msg = response.choices[0].message.content
     st.session_state.messages.append({"role": "assistant", "content": msg})
     st.chat_message("assistant").write(msg)
-
-# def generate_response(input_text):
-#    chat = ChatOpenAI(
-#        model=selected_model,
-#        openai_api_key=os.environ["API_KEY"],
-#        openai_api_base=os.environ["BASE_URL"]
-#    )
-#    st.info(chat.invoke(input_text))
-
-# with st.form("Prompt"):
-#     text = st.text_area(
-#         "Enter text:", "How can I help you?"
-#     )
-#     submitted = st.form_submit_button("Submit")
-#     if submitted:
-#        generate_response(text)
